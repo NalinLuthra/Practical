56e4769aeef5f2d8b7b91d40cfca1afb4d5ff8fc
NalinLuthra <nalin.luthra@gmail.com>
2018-12-16 21:06:46 +0530

    Selection Sort

--- Selection.cpp
+++ Selection.cpp
@@ -8,9 +8,36 @@ void swap(int &a, int &b)
 	b = temp;
 }
 
+int smallest(int a[], int i,int n)
+{
+	int small = a[i];
+	int key = i;
+	for(int j=i;j<n;j++){
+		if(small>a[j]){
+			key = j;
+			small = a[j];
+		}
+	}
+	return key;
+}
 void selection(int a[],int n)
 {
-	for(int i=0;i<n;i++){
-		
+	int key;
+	for(int i=0;i<n;i++)
+		{
+			key = smallest(a,i,n);
+			swap(a[i],a[key]);
+		}
+	for(int i=0;i<5;i++)
+	{
+		cout << a[i] << " " ;
 	}
+}
+
+int main(int argc, char const *argv[])
+{
+	/* code */
+	int a[]={64, 25, 12, 22, 11};
+	selection(a,5);
+	return 0;
 }
