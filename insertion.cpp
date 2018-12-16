6b33e11479ac719eb280d3e693f69875abbafe1a
NalinLuthra <nalin.luthra@gmail.com>
2018-12-16 20:42:23 +0530

    Practical-1

--- insertion.cpp
+++ insertion.cpp
@@ -5,22 +5,24 @@ using namespace std;
 void insertion(int a[],int n)
 {
 	int i,j,key;
-	for(int i=1;i<n;i++)
+	for(i=1;i<n;i++)
 	{
 		
 
 		key = a[i];
 		j = i-1;
 
-		while(j>0 && a[j] > key)
+		while(j>=0 && a[j] > key)
 		{
 			a[j+1] = a[j];
 			j = j-1;
 		}
 		a[j+1] = key;
 	}
-	for(int i=0;i<8;i++){
+	for(int i=0;i<8;i++)
+	{
 		cout << a[i] << " " ;
+	}
 }
 int main(int argc, char const *argv[])
 {
